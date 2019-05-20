024310f9b74f0033e45fea391886deadb2121244
kiran <kiran@DESKTOP-MD6S2SS>
2019-05-21 10:24:11 +1200

    code

---
+++ even odd practice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_odd_practice
+{
+    class Program
+    {
+        //This program will find the number is even or odd//
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter any number");
+            int num = int.Parse(Console.ReadLine());
+            if ((num % 2) == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number is odd");
+            }
+        }
+    }
+}
+
